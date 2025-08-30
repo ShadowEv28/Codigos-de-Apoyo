b586caf52bbfc0794a1fdd11be27d2808650b82c
ShadowEv28 <ae.melladovillegas@gmail.com>
2025-08-30 15:59:42 -0400

    Update Prueba.java

--- CodigosJava/Proyectos/src/Prueba.java
+++ CodigosJava/Proyectos/src/Prueba.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Prueba{
     public static void main(String[] arg){
 
     }
-    public static almacen(){
-        String[] dias = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"}
-        List<String> dia = new ArrayList<>();
+    public static void almacen(){
+        String[] dias = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+        List<String> dia = Arrays.asList();
+        System.out.println(dia);
         
     }
 }
