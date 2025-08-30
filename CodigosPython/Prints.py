f4dd08148b9b8e0bcb94cd7615816d6c31cd2d71
ShadowEv28 <ae.melladovillegas@gmail.com>
2025-08-30 01:27:50 -0400

    cambio

--- CodigosPython/Prints.py
+++ CodigosPython/Prints.py
@@ -6,17 +6,12 @@ print(f"3) Hola {palabra}") #Como pueden ver aqui, pude utilizar la variable "pa
 edad = 21
 año = 2025
 print(f"4) Hola, tengo {edad} y naci el {año - edad}") #Como ven en el codigo, pude calcular el año de nacimiento de forma rapida solo con 2 variables dentro del print y sin necesidad de una tercera variable.
-print("5) Hola \nMundo") #El comando \n sirve para hacer un salto de linea dentro del print.
-print("6) Hola \tMundo") #El comando \t sirve para hacer una tabulacion dentro del print.
-print("7) Hola \"Mundo\"") #El comando \" sirve para poder utilizar comillas dentro de un print que ya esta entre comillas.
-print("8) Hola \'Mundo\'") #El comando \' sirve para poder utilizar comillas simples dentro de un print que ya esta entre comillas dobles.
-print("9) Hola \\Mundo") #El comando \\ sirve para poder utilizar una barra invertida dentro de un print que ya esta entre comillas.
-print("10) Hola Mundo", end=" :)") #El comando end sirve para cambiar el final del print, en este caso en vez de hacer un salto de linea, termina con una carita feliz.
-print("11) Hola Mundo") #Como pueden ver, el print anterior no hizo un salto de linea, sino que termino con una carita feliz.
-print("12) Hola Mundo", sep="-") #El comando sep sirve para cambiar el separador entre varios elementos dentro de un print, en este caso en vez de un espacio, utiliza un guion.
-print("13) Hola", "Mundo", sep="*") #Como pueden ver, el print anterior utilizo un asterisco como separador entre las dos palabras.
-print("14) Hola", "Mundo", sep="***") #Como pueden ver, el print anterior utilizo tres asteriscos como separador entre las dos palabras.
-print("15) Hola", "Mundo", sep="\n") #Como pueden ver, el print anterior utilizo un salto de linea como separador entre las dos palabras.
-print("16) Hola", "Mundo", sep="\t") #Como pueden ver, el print anterior utilizo una tabulacion como separador entre las dos palabras.
-print("17) Hola", "Mundo", sep=" - ") #Como pueden ver, el print anterior utilizo un espacio, un guion y otro espacio como separador entre las dos palabras.
-print("18) Hola", "Mundo", sep=" :) ") #Como pueden ver, el print anterior utilizo una carita feliz con espacios como separador entre las dos palabras.
+print("5) Hola \nMundo") #El comando \n hace un salto a la siguiente linea y imprime lo que esta despues del \n.
+print("6) Hola \tMundo") #El comando \t sirve para hacer una tabulacion, esto se refiere a un espacio largo, puedes probarlo apretando "TAB" tu mismo.
+print("7) Hola \"Mundo\"") #El comando \" sirve para poder utilizar comillas dentro de print.
+print("8) Hola \'Mundo\'") #El comando \' sirve para poder utilizar comillas simples dentro del print.
+print("9) Hola \\Mundo") #El comando \\ sirve para poder utilizar \ dentro del print.
+print("10) Hola Mundo", end=" :)") #El comando end sirve para cambiar lo que seria el salto de linea, en este caso en vez de saltar a la siguiente linea, esta tendra una carita feliz y continuara en la misma linea.
+print("Pueden ver que la linea no salto")
+print("11) Hola Mundo", sep="-") #El comando sep sirve para cambiar el separador entre varios elementos dentro de un print, en este caso en vez de un espacio, utiliza un guion.
+print("hola mun")
