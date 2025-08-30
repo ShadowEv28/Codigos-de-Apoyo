8f59534bb01225300bb7cfe6fd6d32cc48bf670f
ShadowEv28 <ae.melladovillegas@gmail.com>
2025-08-30 15:52:58 -0400

    a

--- CodigosPython/Variables.py
+++ CodigosPython/Variables.py
@@ -24,5 +24,6 @@ strbool = str(Variable_Bool) #Aqui convertimos un bool a String
 strint = str(Variable_Int) #Aqui convertimos un Int a String
 strfloat = str(Variable_Float) #Aqui convertimos un Float a String
 print(strbool + strfloat + strint) #Como ven aqui los valores se juntan unos a otros ya que todos son String ahora, si hubieramos intentado esto antes los valores simplemente se hubieran sumado y daria 21.2
-Variable_List = []
-#Dark es gay
+Variable_List = [] #Las variables List son variables que pueden almacenar multiples valores y de todo tipo.
+Variable_List.append("Dato 1") #En las variables List se pueden añadir valores usando ".append()"
+
